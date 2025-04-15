420acd8cb43e6da7af9f1984d43bb9a649d6d1bc
RusithTharindu <rusiththarindu2@gmail.com>
2025-04-15 13:18:47 +0530

    feat: Add Passport and PreApply components for applicant flow
    - Introduced Passport component as a new page for applicant processing.
    - Added PreApply component with a header for pre-application information.

---
+++ src/app/applicant/passport/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Passport = () => {
+  return <div></div>;
+};
+
+export default Passport;
---
+++ src/app/applicant/passport/pre-apply/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PreApply = () => {
+  return (
+    <div>
+      <h1>Pre Apply</h1>
+    </div>
+  );
+};
+
+export default PreApply;
